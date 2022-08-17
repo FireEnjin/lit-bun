9d0dbe31472e897442bb800eceb2467f23a71d2b
Popcorn245 <bobby@madnesslabs.net>
2022-08-16 23:49:33 -0500

    fix(): Setup package.json and try lit component test

--- hello.ts
+++ hello.ts
@@ -1,6 +1,64 @@
+import { LitElement, html, css } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+@customElement("page-home")
+class PageHome extends LitElement {
+  /**
+   * Copy for the read the docs hint.
+   */
+  @property()
+  docsHint = "Click on the Vite and Lit logos to learn more";
+
+  /**
+   * The number of times the button has been clicked.
+   */
+  @property({ type: Number })
+  count = 0;
+
+  render() {
+    return html`
+      <slot></slot>
+      <ion-card class="card">
+        <ion-button @click=${this._onClick} part="button">
+          count is ${this.count}
+        </ion-button>
+      </ion-card>
+      <p class="read-the-docs">${this.docsHint}</p>
+    `;
+  }
+
+  private _onClick() {
+    this.count++;
+  }
+
+  static styles = css`
+    ion-button {
+    }
+  `;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "page-home": PageHome;
+  }
+}
+
 export default {
   port: 8080,
   fetch(request: Request) {
-    return new Response("Hello FireEnjineer, let's get lit!");
+    return new Response(
+      "<page-home>Hello FireEnjineer, let's get lit!</page-home>",
+      {
+        headers: {
+          "Content-Type": "text/html",
+        },
+      }
+    );
   },
 };
